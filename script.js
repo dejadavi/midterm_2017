4c34232d4fff37bf314a3bf164c390df72149654
Desmond Davis <Dez@Desmonds-MacBook-Air.local>
2017-07-12 14:22:25 -0400

    second commit, trying to get grid to work

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("wuddup doe");
+$(document).ready(function(){
+
+////Set up initial data array on backend
+//populate DOM
+function initialize(){
+
+var $container = $("<div></div>").css("float","left");
+
+for(var i = 0; i < 3; i++) {
+    for (var j = 0; j < 4; j++){
+        $("<div></div>").addClass("box").appendTo($container);
+    }
+    $("<div></div>").css("clear", "both").appendTo($container);
+}
+
+$container.appendTo($("body"));
+console.log($container);
+    
+}
+
+
+initialize();
+
+// function populateDOM(args){
+
+
+// }
+
+
+// ///on click, function checks if seat reserved
+// ///if reserved, fire message
+// ////if not, trigger reserve modal
+// function reserve {
+
+
+
+
+
+// }
+
+
+
+
+
+
+})
