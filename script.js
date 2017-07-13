da07daed49e7c2b0f85408f09dcf90266143717d
jonkrause <jonathonkrause@gmail.com>
2017-07-13 14:03:37 -0400

    logs to array

--- script.js
+++ script.js
@@ -1,88 +1,109 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-////Set up initial data array on backend
-//populate DOM
-function initialize(nRow,nCol){
+    ////Set up initial data array on backend
+    //populate DOM
+    function initialize(nRow, nCol) {
 
-var $container = $("<div></div>").addClass("container");
-var blackOut=[
-    "1,0","1,15"
-    ,"2,0","2,15"
-    ,"3,0","3,1","3,14","3,15"
+        var $container = $("<div></div>").addClass("container");
+        var blackOut = [
+    "1,0", "1,15"
+    , "2,0", "2,15"
+    , "3,0", "3,1", "3,14", "3,15"
 ];
 
-for(var row = 0; row < nRow; row++) { 
-    
-    var $row=$("<div></div>");
-    
-    for (var col = 0; col < (nCol-1); col++){
-        $("<div></div>").addClass(function(){
-            if(blackOut.indexOf(row+","+col)!=-1||row===4){
-                return "blackout"
-            } else {
-                
-                $(this).attr("id",(String.fromCharCode(nRow-row+96)+col).toUpperCase() );
-            
-                return "box"}
-        
-        }).appendTo($row); 
-        
-        
+        for (var row = 0; row < nRow; row++) {
+
+            var $row = $("<div></div>");
+
+            for (var col = 0; col < (nCol - 1); col++) {
+                $("<div></div>").addClass(function () {
+                    if (blackOut.indexOf(row + "," + col) != -1 || row === 4) {
+                        return "blackout"
+                    } else {
+
+                        $(this).attr("id", (String.fromCharCode(nRow - row + 96) + col).toUpperCase());
+
+                        return "box"
+                    }
+
+                }).appendTo($row);
+
+
+
+            }
+            $("<div></div>").css("clear", "both").appendTo($row);
+
+            $row.prependTo($container);
+
+
+
+        }
+
+
+
+
+        ////Add letters, append to id and display
+        $container.appendTo($("body"));
+
 
     }
-    $("<div></div>").css("clear", "both").appendTo($row);
-    
-    $row.prependTo($container);
-    
-   
-
-}
-    
-
-    
-    
-////Add letters, append to id and display
-$container.appendTo($("body"));
-
-    
-}
 
 
-function addId(row, col){
+    function addId(row, col) {
 
-$(".box").attr("id",row+col).text(row+col);
+        $(".box").attr("id", row + col).text(row + col);
 
-}
+    }
 
-initialize(6,17);
+    initialize(6, 17);
 
-// function populateDOM(args){
-
-
-// }
+    // function populateDOM(args){
 
 
-// ///on click, function checks if seat reserved
-// ///if reserved, fire message
-// ////if not, trigger reserve modal
-// function reserve {
+    // }
+
+
+    // ///on click, function checks if seat reserved
+    // ///if reserved, fire message
+    // ////if not, trigger reserve modal
+    // function reserve {
 
 
 
 
 
-// }
+    // }
 
-    $('.box').on('click', function (){
-     $(this).toggleClass('reserve');
+
+    var array01 = [];
+    array01.splice(0, 0);
+    console.log(array01);
+
+
+
+
+
+
+    $('.box').on('click', function () {
+        $(this).toggleClass('reserve');
         console.log($(this).attr("id"));
-        $('#seat').append($(this).attr("id") + " ");
+        $('#seat').append(array01.indexOf($(this).attr("id")));
         
+        if (array01.indexOf($(this).attr("id")) === -1) {
+
+            array01.push($(this).attr('id'));
+
+        } else {
+            array01.indexOf($(this).attr("id"));
+            array01.splice(array01.indexOf($(this).attr("id"), 1));
+            console.log(array01);
+
+        }
     });
-    
-    
 
 
+
+    console.log(array01);
 
 
 });
