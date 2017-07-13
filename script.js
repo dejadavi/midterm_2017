da5ae8ce56ef2b33fdce1750d9e615cd443a1515
Emily Webber <webber.emilya@gmail.com>
2017-07-13 12:58:39 -0400

    adding booking details

--- script.js
+++ script.js
@@ -2,24 +2,53 @@ $(document).ready(function(){
 
 ////Set up initial data array on backend
 //populate DOM
-function initialize(){
+function initialize(nRow,nCol){
 
-var $container = $("<div></div>").css("float","left");
+var $container = $("<div></div>").addClass("container");
+var blackOut=[
+    "1,0","1,15"
+    ,"2,0","2,15"
+    ,"3,0","3,1","3,14","3,15"
+];
 
-for(var i = 0; i < 3; i++) {
-    for (var j = 0; j < 4; j++){
-        $("<div></div>").addClass("box").appendTo($container);
+for(var row = 0; row < nRow; row++) {
+
+    var $row=$("<div></div>");
+
+    for (var col = 0; col < (nCol-1); col++){
+        $("<div></div>").addClass(function(){
+            if(blackOut.indexOf(row+","+col)!=-1||row===4){
+                return "blackout"
+            } else {
+                $(this).attr("id",(String.fromCharCode(nRow-row+96)+col).toUpperCase() );
+
+                return "box"}
+
+        }).appendTo($row);
+
+
     }
-    $("<div></div>").css("clear", "both").appendTo($container);
-}
+    $("<div></div>").css("clear", "both").appendTo($row);
 
+    $row.prependTo($container);
+
+
+
+}
+////Add letters, append to id and display
 $container.appendTo($("body"));
-console.log($container);
-    
+
+
 }
 
 
-initialize();
+function addId(row, col){
+
+$(".box").attr("id",row+col).text(row+col);
+
+}
+
+initialize(6,17);
 
 // function populateDOM(args){
 
@@ -43,4 +72,4 @@ initialize();
 
 
 
-})
+});
